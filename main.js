78bb4e36df29874ff98321e7f149165285d19942
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2023-04-26 20:39:17 +0200

    optimized code

--- main.js
+++ main.js
@@ -1,67 +1,66 @@
-const divs = document.getElementsByTagName('div');
-let textField = document.getElementById('text');
+const form = document.querySelector('.form')
+const input = document.querySelector('#date-input')
+const content_container = document.querySelector('.content-container')
+const data =
 
-let yearsDiv = document.getElementById('years');
-let monthsDiv = document.getElementById('months');
-let weeksDiv = document.getElementById('weeks');
-let daysDiv = document.getElementById('days');
-let hoursDiv = document.getElementById('hours');
-let minutesDiv = document.getElementById('minutes');
-let secondsDiv = document.getElementById('seconds');
-
-let years;
-let months;
-let weeks;
-let days;
-let hours;
-let minutes;
-let seconds;
-
-
-
-function getBirthDate(){
-  let birthDate = textField.value;
-  textField.value = '';
-  calcTimeLived(birthDate);
+{
+  years: Number,
+  months: Number,
+  weeks: Number,
+  days: Number,
+  hours: Number,
+  minutes: Number,
+  seconds: Number
 }
 
 
+form.addEventListener('submit', (event) => {
+  content_container.replaceChildren()
+  event.preventDefault()
+  const date = Date.parse(input.value)
+  livedTime(date)
+  display()
+})
 
-function calcTimeLived(birthdate){
-  
-  let birthday = new Date(birthdate);
-  let ageDifMs = Date.now() - birthday.getTime(); 
-  let date = new Date(ageDifMs);
-  seconds = date / 1000;
-  minutes = seconds / 60;
-  hours = minutes / 60;
-  days = hours / 24;
-  weeks = days / 7;
-  months = weeks / 4.345;
-  years = months / 12;
-
-  render();
-}
-
-function render(){
-  yearsDiv.innerHTML = `${Math.round(years)} years`.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  monthsDiv.innerHTML =`${Math.round(months)} months `.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  weeksDiv.innerHTML = `${Math.round(weeks)} weeks `.replace(/\B(?=(\d{3})+(?!\d))/g, ","); 
-  daysDiv.innerHTML = `${Math.round(days)} days `.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  hoursDiv.innerHTML = `${Math.round(hours)} hours `.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  minutesDiv.innerHTML = `${Math.round(minutes)} minutes `.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  secondsDiv.innerHTML = `${Math.round(seconds)} seconds `.replace(/\B(?=(\d{3})+(?!\d))/g, ","); 
-}
-
-
-function setLayout() {
-  let i = 0;
-
-  while (i < divs.length) {
-    divs[i].style.height = (100 / divs.length) + 'vh';
-    i++;
+const display = () => {
+  for (const value in data) {
+    let element = document.createElement('div')
+    element.classList.add('element')
+    element.textContent = `${value} : ${data[value]}`
+    content_container.appendChild(element)
   }
+}
 
+
+
+
+
+
+
+
+const livedTime = (birthdate) => {
+  let birthday = new Date(birthdate); //Set Date
+  let diff = Date.now() - birthday.getTime(); //get difference between dates
+  let diffDate = new Date(diff); //set new date
+
+  const seconds = diffDate / 1000;
+  const minutes = seconds / 60;
+  const hours = minutes / 60;
+  const days = hours / 24;
+  const weeks = days / 7;
+  const months = weeks / 4.345;
+  const years = months / 12;
+
+  data.days = Math.floor(days);
+  data.hours = Math.floor(hours);
+  data.minutes = Math.floor(minutes);
+  data.months = Math.floor(months);
+  data.seconds = Math.floor(seconds);
+  data.weeks = Math.floor(weeks);
+  data.years = Math.floor(years);
 
 }
+
+
+
 
